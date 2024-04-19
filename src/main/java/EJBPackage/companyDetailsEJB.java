8cf5b328536b7c39643307f0d2b9aeeabf40b115
Naitik Patel <pnaitik504@gmail.com>
2024-04-19 22:34:30 +0530

    CompanyDetailsEJB

---
+++ src/main/java/EJBPackage/companyDetailsEJB.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package EJBPackage;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Admin
+ */
+@Stateless
+public class companyDetailsEJB {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
